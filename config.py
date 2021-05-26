5de31e3475abe74079f09a7b3944a8aafd2402e3
Josh Lipton <josh.lipton@outlook.com>
2021-05-26 12:08:05 -0400

    added comments and updated eventstream logic

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Incoming Webhooks URL
 url = "https://webexapis.com/v1/webhooks/incoming/XXXXX"
 # amp Events Creds
-amp_client_id = 'b5712932a14613fe2d58'
+amp_client_id = 'd17c51182b3f89c04cc6'
 
-amp_api_key = '7115a01e-48a9-4b7c-a3ea-63878362518f'
+amp_api_key = 'ea534be1-a3f4-4bb8-b989-681a5456d987'
 
 amp_url = 'https://api.amp.cisco.com/v1/events'
