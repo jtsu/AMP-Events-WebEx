798aac32d518be43f50285d2eb639a10e1c6f153
Jason <jtsu@cisco.com>
2021-07-15 09:50:38 -0700

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,9 @@
 amp_client_id = 'XXX'
 amp_api_key = 'XXX-XXX-XXX'
 
-#webex room ID
+#webex room ID and token
 webex_room_id = 'XXXXX'
-
-# set env variable for webex access token
-# export WEBEX_TEAMS_ACCESS_TOKEN=XXXXX-XX-XXX
+webex_access_token = 'XXXXXXX'
 
 #Event Stream API
 amp_stream_url = 'https://api.amp.cisco.com/v1/event_streams'
